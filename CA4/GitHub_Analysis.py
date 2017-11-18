7686319fee711379c7d7cde0cbf825595829528a
unknown <PauliusZaicev>
2017-11-18 13:14:57 +0000

    Updated python script to return pandas dataframe

--- CA4/GitHub_Analysis.py
+++ CA4/GitHub_Analysis.py
@@ -23,12 +23,25 @@ class Commit (object):
         self.change_path = change_path
         self.comment = comment
         
+    #create instance which return commits as a dictionary
+    def to_dic(self):
+        return {'revision': self.revision, 'author': self.author, 'date': self.date, 'time': self.time, 'number of lines': self.number_of_lines , 'changed path': self.change_path, 'comment': self.comment}
+    
     #return instance variable
     #return number of lines as a string, and join comments (because we can have multiple lines comments)
     def __repr__ (self):
         return self.revision+ ',' + self.author + ',' + self.date + \
-    ',' + self.time + ',' + str(self.number_of_lines) + ',' + ' '.join(self.comment) + '\n'
+    ',' + self.time + ',' + str(self.number_of_lines) + ',' + ' '.join(self.change_path) + ',' + ' '.join(self.comment) + '\n'
 
+
+
+import pandas as pd
+#create function which returns dataframe of commits
+def to_df(data):
+    commits = []
+    for commit in data:
+        commits.append(commit.to_dic())
+    return pd.DataFrame(commits)
 
 #get commits function will help us to break inforamtion from each commit       
 def get_commits(data):
@@ -53,21 +66,17 @@ def get_commits(data):
                 #get number of lines and return as int
                 int(details[3].strip().split(' ')[0]))
             #set file end index
-            # in my oppinion look for empty line and for this lines index adds one
-            
-            #ask darren to explain, but how each commit is treated seperatelly
+            # build in function look s for empty line index and return that index (use build in index function)
             change_file_end_index = data.index ('', index + 1)
             #chnaged path line starts from the third line
-            commit.changed_path = data[index +3 : change_file_end_index]
+            commit.change_path = data[index +3 : change_file_end_index]
             #get comments
-            #
             commit.comment = data [change_file_end_index + 1:
                 change_file_end_index + 1 + commit.number_of_lines]
             # add details to the list of commits.
             commits.append(commit)
             #find for a next 72 hifins
-            
-            #ask to explain
+            #look for next index in the file which starts with 72 hifins
             index = data.index(sep, index + 1)
         except IndexError:
             #then index equal to len of data program stops
@@ -91,87 +100,72 @@ def save_commits(commits, any_file):
 if __name__ == '__main__':
     # open the file - and read all of the lines.
     changes_file = 'changes_python.log'
+    #assigne all lines to data variable
     data = read_file(changes_file)
+    #get commits from get _commits function and inert data variabel
     commits = get_commits(data)
-    
+    #make commits as a data frame 
+    data_frame = to_df(commits)
+    data_frame['author'][9]
+    #going to test latter
     #save_commits(commits, 'changes.csv')
     
     # print the number of lines read
     print(len(data))
-    #print(commits)
+    #print lenght of commits, from thiscommits variable will start forming my data set
     print(len(commits))
-    print(commits[0])
+    #display 0 index from the commit list
+    print(commits[5])
+    #print author from the 0 index commit list
     print(commits[0].author)
+    #print author from the 420 index commit list
     print(commits[420].author)
+    #print revision from the 0 index commit list
     print(commits[0].revision)
+    #print comment from the 23 index commit list
     print (commits[23].comment)
-    print (commits[40].changed_path)
-
-
-'''
-
-#import required libraries
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
-#Seaborn is a Python visualization library based on matplotlib. 
-#It provides a high-level interface for drawing attractive statistical graphics.
-import seaborn as sns
-plt.style.use('fivethirtyeight')
-import warnings
-warnings.filterwarnings('ignore')
-
-
-
-commits = get_commits(data)
-
-#import pandas
-import pandas as pd
-
-#make commits as a data frame
-data_frame = pd.DataFrame(commits)
-
-#dispaly data frame
-print data_frame
-
-
-
-#display information about data set
-data_frame.info()
-
-#display head of the data set
-data_frame.head()
-
-#print unique authors
-data_frame['author'].unique()
-
-#count how many different outhors have ogged in
-log_in_per = data_frame[['author' ,'date']].groupby(['author'], as_index = False).count()
-print log_in_per
-#create plot
-sns.factorplot('author', 'date', data = data_frame, size = 4, aspect = 3)
-
-#create tree axes for plots
-fig, (axis1) = plt.subplots(1,1,figsize=(15,5))
-
-sns.barplot(x = 'author', y = 'date', data = log_in_per, order = ['Thomas', 'Vincent', '/OU=Domain Control Validated/CN=svn.company.net', 'Jimmy','Freddie', 'Dave', 'ajon0002', 'murari.krishnan', 'Nicky', 'Alan'], ax = axis1)
-'''
 
     
-'''
-if __name__ == '__main__':
-    # open the file - and read all of the lines.
-    changes_file = 'changes_python.log'
-    data = read_file(changes_file)
-    commits = get_commits(data)
+    #data_frame.head()['changed path']
 
-    # print the number of lines read
-    print(len(data))
-    #print(commits)
-    print(len(commits))
-    print(commits[0])
-    print(commits[0].author)
-    print(commits[420].author)
-    print(commits[0].revision)
-    save_commits(commits, 'changes.csv')'''
+    #import required libraries
+    import pandas as pd
+    import numpy as np
+    import matplotlib.pyplot as plt
+    #Seaborn is a Python visualization library based on matplotlib. 
+    #It provides a high-level interface for drawing attractive statistical graphics.
+    import seaborn as sns
+    plt.style.use('fivethirtyeight')
+    import warnings
+    warnings.filterwarnings('ignore')
+    
+    #print commits
+
+'''
+    #display data frame
+    print data_frame.head()
+
+    
+    
+    #display information about data set
+    data_frame.info()
+    
+    #display head of the data set
+    data_frame.head()
+    
+    #print unique authors
+    data_frame['author'].unique()
+    
+    
+    #count how many different outhors have ogged in
+    log_in_per = data_frame[['author' ,'date']].groupby(['author'], as_index = False).count()
+    print log_in_per
+    #create plot
+    sns.factorplot('author', 'date', data = data_frame, size = 4, aspect = 3)
+    
+    #create tree axes for plots
+    fig, (axis1) = plt.subplots(1,1,figsize=(15,5))
+    
+    sns.barplot(x = 'author', y = 'date', data = log_in_per, order = ['Thomas', 'Vincent', '/OU=Domain Control Validated/CN=svn.company.net', 'Jimmy','Freddie', 'Dave', 'ajon0002', 'murari.krishnan', 'Nicky', 'Alan'], ax = axis1)
+    '''
+
