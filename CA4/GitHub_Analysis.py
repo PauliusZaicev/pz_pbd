2cd9528e18432148b1d669e9556b0645969555e6
unknown <PauliusZaicev>
2017-11-19 10:59:53 +0000

    Third and fourth statistics added

--- CA4/GitHub_Analysis.py
+++ CA4/GitHub_Analysis.py
@@ -89,7 +89,7 @@ def save_commits(commits, any_file):
     #open file as write
     my_file = open (any_file, 'w')
     #set column names
-    my_file.write('revision,author,date, time,number_of_lines,comment\n')
+    my_file.write('revision,author,date, time,number_of_lines,changed_path,comment\n')
     #for commit in commits write each line as a string
     for commit in commits:
        my_file.write(str(commit)) 
@@ -108,7 +108,7 @@ if __name__ == '__main__':
     data_frame = to_df(commits)
     
     #going to test latter
-    #save_commits(commits, 'changes.csv')
+    save_commits(commits, 'changes.csv')
     
     # print the number of lines read
     print(len(data))
@@ -159,19 +159,130 @@ if __name__ == '__main__':
     data_frame['author'].unique()
 
 
-    #count how many different authors have logged in
+    #count how many different authors have logged in and groub by author
     log_in_per = data_frame[['author' ,'date']].groupby(['author'], as_index = False).count()
+    #rename column names and print
     log_in_per = log_in_per.rename(columns = {'author': 'Author', 'date': 'Log in count'})
-    print log_in_per
+    #sort values
+    log_in_per = log_in_per.sort_values(['Log in count'], ascending = [False])
+    log_in_per
     
     
+    #create a factor plot
+    sns.factorplot('Author', 'Log in count', data = log_in_per, size = 4, aspect = 3)
     
-    #create plot
-    sns.factorplot('author', 'date', data = data_frame, size = 4, aspect = 3)
-    
-    #create tree axes for plots
+    #create an axe for plots
     fig, (axis1) = plt.subplots(1,1,figsize=(15,5))
     
-    sns.barplot(x = 'author', y = 'date', data = log_in_per, order = ['Thomas', 'Vincent', '/OU=Domain Control Validated/CN=svn.company.net', 'Jimmy','Freddie', 'Dave', 'ajon0002', 'murari.krishnan', 'Nicky', 'Alan'], ax = axis1)
-    '''
+    #display as a bar chart
+    ax = sns.barplot('Author', 'Log in count', data = log_in_per, order = ['Thomas', 'Vincent', 'DCV', 'Jimmy','Freddie', 'Dave', 'ajon0002', 'murari.krishnan', 'Nicky', 'Alan'], ax = axis1)
+    ax.set (xlabel = 'Author', ylabel = 'Log in count')
+    plt.show
 
+
+    #check how many comment lines each author has created
+    number_of_lines = data_frame[['author' ,'number of lines']].groupby(['author'], as_index = False).sum()
+    #rename columns
+    number_of_lines= number_of_lines.rename(columns = {'author': 'Author', 'number of lines': 'Commented lines'})
+    #sort values in descending order
+    number_of_lines = number_of_lines.sort_values(['Commented lines'], ascending = [False])
+    number_of_lines
+    
+    
+    #merge two different data frames
+    mergged_df = pd.merge( left = log_in_per, right = number_of_lines, left_on = 'Author', right_on = 'Author' )
+    mergged_df
+    
+    #add new column to the dataframe
+    mergged_df['Avarage number of comments per commit'] = pd.Series.round(mergged_df['Commented lines'] / mergged_df['Log in count'], 2)
+    mergged_df
+    
+    
+    #create a factor plot
+    sns.factorplot('Author', 'Avarage number of comments per commit', data = mergged_df, size = 4, aspect = 3)
+    
+    
+    #statistic 3
+    data_frame
+    
+    #add new collumn
+    #data_frame['hour_split'] = data_frame['time'].str.split(':')
+    
+    #access index hours
+    #data_frame['hour'] = data_frame['time'].str.split(':',1).str[0]
+    
+    #strip time into hours minutes and seconds
+    data_frame['hours'], data_frame['minutes'], data_frame['seconds'] = data_frame['time'].str.split(':',2).str
+    
+    
+    #add additional column in hte dataframe, 0 first half of the day 1 second half of the day
+    data_frame['day_time'] = data_frame['hours']
+    
+    #repalce first half of the day values to 0
+    data_frame['day_time'] = data_frame['day_time'].replace(['00', '01', '02', '03', '04', \
+              '05','06','07','08','09','10','11'], '0')
+                
+    #repalce second half of the day values to 1
+    data_frame['day_time'] = data_frame['day_time'].replace(['12', '13', '14', '15', '16', \
+          '17', '18','19','20','21','22','23', '24'], '1')
+    
+                
+    #count how many commits done in the morning and in the evening
+    count_daytime = data_frame[['day_time' ,'revision']].groupby(['day_time'], as_index = False).count()
+
+    #create sub plots
+    fig, (axis1,axis2) = plt.subplots(1,2,figsize=(15,6))
+    
+    #create a count plot how many times what time authors had made commits
+    ax = sns.countplot(x='day_time', data=data_frame, ax=axis1, hue = 'day_time')
+    ax.set (xlabel = 'Period of the day', ylabel = 'Log in count')
+    plt.show
+    
+    #count how many comments made in the morning and howe many in the evening
+    number_of_lines_time = data_frame[['day_time' ,'number of lines']].groupby(['day_time'], as_index = False).sum()
+    number_of_lines_time 
+
+    ax = sns.barplot(x='day_time', y='number of lines', data=number_of_lines_time, ax=axis2, order=['0','1'])
+    ax.set (xlabel = 'Period of the day', ylabel = 'Number of commented lines')
+    plt.show
+    
+    
+    fig, (axis1,axis2) = plt.subplots(1,2,figsize=(15,6))
+        
+    ax = count_daytime.plot.pie(y='revision', ax = axis1)   
+    ax.set ( ylabel = 'Log in count')
+    plt.show
+    
+    ax = number_of_lines_time.plot.pie(y='number of lines', ax = axis2)   
+    ax.set ( ylabel = 'Number of commented lines')
+    plt.show
+
+    #amount of comments in the morning and night
+    #amlount of log ins in the morning and night
+    #overall amount of 
+                
+
+    '''doesn't work with 09 values'''
+    
+    '''
+    data_frame['day_period'] = data_frame['hours']
+    data_frame['day_period'].loc[data_frame['hours'] < 12] = 0
+    data_frame['day_period'].loc[data_frame['hours'] >= 12] = 1
+    
+    
+    #add additional cell in the dqata frame which will have first string value
+    data_frame ['hour_first_index'] = data_frame['hours'].str[0]
+
+    data_frame['day_period_first_index'] = data_frame ['hour_first_index']
+    data_frame['day_period_first_index'].loc[data_frame['hour_first_index'] == 0] = 0
+
+    del data_frame['hour_first_index']
+    
+    
+    '''
+    
+
+    
+    
+    
+    
